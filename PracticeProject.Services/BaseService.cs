a8d0522488cc403e580735ec8a515788b42e1dbb
Patty Pierson <ppierson@gmail.com>
2017-09-13 14:55:12 -0700

    redo earlier changes

--- PracticeProject.Services/BaseService.cs
+++ PracticeProject.Services/BaseService.cs
@@ -1,25 +1,7 @@
-﻿using PracticeProject.Data;
-using PracticeProject.Data.Providers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PracticeProject.Services
+﻿namespace PracticeProject.Services
 {
     public abstract class BaseService
     {
-        public IDataProvider DataProvider { get; set; }
-
-        public BaseService(IDataProvider dataProvider)
-        {
-            this.DataProvider = dataProvider;
-        }
-
-        public BaseService()
-        {
-            this.DataProvider = new SqlDataProvider("Data Source=localhost;Initial Catalog=PracticeProject;Integrated Security=True");
-        }
+        protected string connString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
     }
 }
