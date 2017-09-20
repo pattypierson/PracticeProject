d063b015d12e1ebe310007417cdc9cef7fe39074
Patty Pierson <ppierson@gmail.com>
2017-09-20 09:50:23 -0700

    Added a team table

---
+++ PracticeProject.Database/Tables/Team.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Team]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [TeamName] NVARCHAR(50) NOT NULL, 
+    [CreatedBy] NVARCHAR(128) NOT NULL, 
+    [CreatedDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [ModifiedDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
