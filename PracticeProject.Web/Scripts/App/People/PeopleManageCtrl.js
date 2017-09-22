f468c24153029fb04a9a2ddd1f1170c04520b7f5
Patty Pierson <ppierson@gmail.com>
2017-09-21 21:36:17 -0700

    Added manage cshtml for people

---
+++ PracticeProject.Web/Scripts/App/People/PeopleManageCtrl.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    console.log("ppl manage controller works");
+    "use strict";
+
+    angular
+        .module("mainApp")
+        .controller("peopleManageCtrl", peopleManageCtrl);
+
+    peopleManageCtrl.$inject = ["$scope", "peopleService"];
+
+    function peopleManageCtrl($scope, peopleservice) {
+
+    }
+
+
+})();
