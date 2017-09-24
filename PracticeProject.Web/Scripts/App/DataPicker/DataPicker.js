ad5f0984d51bf8f4aa945cc2af8306d36e91a38b
Patty Pierson <ppierson@gmail.com>
2017-09-23 23:17:55 -0700

    Got randomizer to work with hardcoded values.

--- PracticeProject.Web/Scripts/App/DataPicker/DataPicker.js
+++ PracticeProject.Web/Scripts/App/DataPicker/DataPicker.js
@@ -28,11 +28,37 @@
         function _peopleGetAllSuccess(response) {
             vm.items = response.data.items;
             console.log(vm.items);
+            //names = vm.items.firstName;
+            //console.log(names);
         }
 
         //--Get All ERROR--
         function _peopleGetAllError(error) {
             console.log(error);
         }
+
+        //--RANDOMIZER--
+
+        $('button').click(function () {
+            $('h4').text(randomEl(names) + ' ' + randomEl(cuisines));
+            selectElementContents($('h4')[0]);
+        });
+
+        function randomEl(list) {
+            var i = Math.floor(Math.random() * list.length);
+            return list[i];
+        }
+
+        function selectElementContents(el) {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        }
+
+        var names = ["test", "test2", "test3"];
+
+        var cuisines = ["American", "Thai", "Pizza", "Burgers"];
     }
 })();
