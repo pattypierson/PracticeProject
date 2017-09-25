824cbd1a4e095718ec6cf0ba987f6d226035e063
Patty Pierson <ppierson@gmail.com>
2017-09-25 15:05:46 -0700

    created a new stored proc

---
+++ PracticeProject.Database/StoredProcedures/Images_SelectById.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[Images_SelectById]
+	@Id int
+AS
+/*
+	declare @_id int = 1
+	exec [dbo].[Images_SelectById] @_id
+*/
+BEGIN
+	SELECT 
+		 [Image]
+	FROM [dbo].[Images]
+	WHERE Id = @Id
+END
